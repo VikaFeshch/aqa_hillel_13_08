8edfeddbbe163fe8f6214fff68e69c88bd7d9f10
VictoriiaFeshchenko <vikafeshch@gmail.com>
2024-09-01 17:26:52 +0300

    added HW 06_2

---
+++ lesson_06/homework_06_2.py
@@ -0,0 +1,8 @@
+while True:
+    if "h" in input("Input your word that contains 'h' or 'H' ").lower():
+        print("Thanks")
+        break
+    else: print("inputed word doesn'n contain letters 'h' or 'H', try again ")
+
+
+
