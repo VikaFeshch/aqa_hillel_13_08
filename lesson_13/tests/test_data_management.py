1121aa5e1fe561fc1837afcdf2274370dc1e2bf7
VictoriiaFeshchenko <vikafeshch@gmail.com>
2024-09-29 18:31:36 +0300

    added lesson_13

---
+++ lesson_13/tests/test_data_management.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+def read_test_data(file_name):
+    test_data_path = Path("../data") / file_name
+    test_data_path = test_data_path.absolute()
+    print(test_data_path)
+    if test_data_path.exists() and test_data_path.is_file():
+        return test_data_path.read_text()
+
+def test_data_management():
+    data = read_test_data("some_data.txt")
+    assert "John1" in data
