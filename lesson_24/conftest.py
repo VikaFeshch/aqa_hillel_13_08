2298acbb1ccd1a35bb2452d0618a02b2a5e13bf5
VictoriiaFeshchenko <vikafeshch@gmail.com>
2024-11-11 22:52:16 +0200

    added lesson_24

---
+++ lesson_24/conftest.py
@@ -0,0 +1,25 @@
+# import pytest
+#
+# from lesson_24.src.app import App
+#
+#
+# def pytest_addoption(parser):
+#     parser.addoption("--env", action="store", default="qa", help="Env to run test against (qa, prod)")
+#     parser.addoption("--test-id", action="store", default=1, help="Some test id")
+#
+# # @pytest.fixture(scope="session")
+# # def database():
+# #     db = DbConnection()
+# #     yield db
+# #     db.close()
+#
+# @pytest.fixture(scope="module")
+# def app(request):
+#     env = request.config.getoption("--env")
+#     return App(env=env)
+#
+#
+# @pytest.fixture(scope="module")
+# def app_auth(request):
+#     env = request.config.getoption("--env")
+#     return App(env=env, api_key="special-key")
---
+++ lesson_24/src/app.py
@@ -0,0 +1,29 @@
+import configparser
+import logging
+from pathlib import Path
+
+from lesson_24.src.pet_api import PetApi
+from lesson_24.src.store_api import StoreApi
+from lesson_24.src.user_api import UserApi
+
+
+class App:
+    def __init__(self, env="qa", api_key: str = None):
+        self.logger = logging.getLogger(__name__)
+
+        config = configparser.ConfigParser()
+        config_path = Path(__file__).resolve().parent.parent / "settings.ini"
+        config.read(config_path)
+        if env in config:
+            base_url = config[env]["BASE_URL"]
+        else:
+            raise ValueError(f"Environment is not supported {env}")
+
+        config = {section: dict(config[section]) for section in config.sections()}
+        config["api_key"] = api_key
+        config["base_url"] = base_url
+        config["logger"] = self.logger
+
+        self.pet = PetApi(config)
+        self.user = UserApi(config)
+        self.store = StoreApi(config)
